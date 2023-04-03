35eddfeb79a513da68e1c396a3623a127bbe6a1d
deniz atmaca <deniz.atmaca@outlook.com>
2023-04-04 00:15:36 +0300

    week 3

---
+++ week3RemainderOperator/Program.cs
@@ -0,0 +1,27 @@
+﻿//Odd or even
+
+int a = 11;
+
+int result = 11 % 2;
+
+Console.WriteLine(result);
+
+//if (result == 0)
+//{
+//    Console.WriteLine($"{a} is an even number");
+//}else
+//{
+//    Console.WriteLine($"{a} is an odd number");
+//}
+
+switch(result)
+{
+    case 0:
+        Console.WriteLine($"{a} is an even number");
+        break;
+        case 1: Console.WriteLine($"{a} is an odd number");
+        break;
+    default:
+        Console.WriteLine("Invalid value");
+        break;
+}
